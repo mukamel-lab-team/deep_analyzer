2e402f3d2123a2d5ff10f80e5907623a9fec9cf3
Diego Zamora <Diego Zamora>
2017-08-13 03:18:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# Training log analyzer for the Deepcpg experiments
+
+### version 1.0.2
+
+Diego Zamora Rodríguez <dzamora@ucsd.edu>
+
+
+
+## Instructions to run 
+
+
+- Install python 3
+
+[Download Python for your system](https://www.python.org/downloads/)
+
+- Install plotyly
+
+``` pip install matplotlib ```
+
+- Install numpy
+
+[Instructions](https://docs.scipy.org/doc/numpy-1.10.1/user/install.html)
+
+
+- Run it
+
+To run it you should indicate the log file as follows  
+
+``` python plot.py <path/to/logfile> ```
+
+for example
+
+``` python plot.py train.log ```
+
+# Notes
+
+We strongly recommend you to install these dependencies using Anaconda or Miniconda for example ``` conda install numpy ```
+
+If any bug appears please email me
---
+++ plot.py
@@ -0,0 +1,110 @@
+"""
+Diego Zamora Rodríguez <dzamora@ucsd.edu>
+
+Training log analyzer for the Deepcpg experiments
+
+version 1.0.2
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+def find_data(filename, text):
+    file = open(filename, "r")
+    i = 0
+    for line in file:
+        i = i + 1
+        if line.find(text) == 0:
+            print("Data found in line " + str(i+2))
+            return i+2
+    file.close()
+
+
+def get_values(filename, start):    
+    loss = []
+    acc = []
+    file = open(filename, "r")
+    for i, line in enumerate(file):        
+        l = line.split()
+        try:
+            if i > (start-1):
+                loss.append(l[0])
+                acc.append(l[2])
+                if l[0] == "Validation":
+                    break
+        except:
+            print("\n")
+    file.close()
+    if loss[-1] == "Validation" or acc[-1]  ==  "performance:":
+        loss.pop()
+        acc.pop()
+    print("============== Values ==============")
+    print(loss)
+    print(acc)
+    return [loss, acc]
+
+
+def plot_acc(training, validation):
+    
+    X = np.array(list(range(30)))  # epoch number
+    Y1 = np.array(training)
+    Y2 = np.array(validation)
+
+    plt.title('Accuracy performance visualization')
+    plt.ylabel('Performance')
+    plt.xlabel('Number of Epoch')
+
+    plt.xlim(0, 30)
+    plt.ylim(0, 4)
+
+    plt.grid(True)
+
+    plt.plot(X, Y1, 's', color="red", label="Training")
+    plt.plot(X, Y2, 's', color="blue", label="Validation")
+
+    plt.legend()
+    plt.show()
+
+
+def plot_loss(training, validation):
+    
+    X = np.array(list(range(30)))  # epoch number
+    Y1 = np.array(training)
+    Y2 = np.array(validation)
+
+    plt.title('Loss performance visualization')
+    plt.ylabel('Performance')
+    plt.xlabel('Number of Epoch')
+
+    plt.xlim(0, 30)
+    plt.ylim(0, 4)
+
+    plt.grid(True)
+
+    plt.plot(X, Y1, 's', color="red", label="Training")
+    plt.plot(X, Y2, 's', color="blue", label="Validation")
+
+    plt.legend()
+    plt.show()
+
+
+
+def startup(filename):
+    training = "Training set performance:"
+    validation = "Validation set performance:"
+
+    training_start = find_data(filename, training)
+    validation_start = find_data(filename, validation)
+
+    training_values = get_values(filename, training_start)
+    validation_values = get_values(filename, validation_start)
+
+    plot_loss(training_values[0], validation_values[0])
+    plot_acc(training_values[1], training_values[1])    
+
+
+if __name__ == "__main__":
+    startup(sys.argv[1])
